0fc24d0b1ee47bf7a945023d51e3810a97a35424
sheiserman <heiserman@apple.com>
2022-09-04 16:52:57 -0700

    fix

--- ts_source/preprocess/preprocess.py
+++ ts_source/preprocess/preprocess.py
@@ -4,9 +4,8 @@ def split_data(data, data_cap: int, time_col: str, features_inout: dict, test_pr
     data = data[:data_cap]
     train_prop = 1-test_prop
     train_split, test_split = int(train_prop*100), int(test_prop*100)
-    if not train_models:
-        train_split, test_split = 0, 100
-    print(f'Spltting data Time0 / Time1 by {train_split} / {test_split}...')
+    if train_models:
+        print(f'Spltting data Time0 / Time1 by {train_split} / {test_split}...')
     t0_endrow = int(data.shape[0]*train_prop)
     features_all = list(set(features_inout['in']+features_inout['pred']))
     if time_col:
@@ -16,6 +15,7 @@ def split_data(data, data_cap: int, time_col: str, features_inout: dict, test_pr
     x_t0, y_t0 = t0[features_inout['in']+[time_col]], t0[features_inout['pred']+[time_col]]
     x_t1, y_t1 = t1[features_inout['in']+[time_col]], t1[features_inout['pred']+[time_col]]
     dict_data = {'x_t0':x_t0, 'x_t1':x_t1, 'y_t0':y_t0, 'y_t1':y_t1, 't0':t0, 't1':t1, 't0t1': pd.concat([t0,t1], axis=0)}
-    for dname, d in dict_data.items():
-        print(f"  {dname} = {d.shape}")
+    if train_models:
+        for dname, d in dict_data.items():
+            print(f"  {dname} = {d.shape}")
     return dict_data
